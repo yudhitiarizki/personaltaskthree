f0972f018985447838d22d4111d84e2a8f16b1b0
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2022-11-02 21:58:20 +0700

    first commit

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -24,7 +24,7 @@ const Detail = () => {
             <StButton
               borderColor="#ddd"
               onClick={() => {
-                navigate("/");
+                navigate("/personaltaskthree");
               }}
             >
               Kembali
